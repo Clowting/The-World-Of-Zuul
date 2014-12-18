f1cd2023e7d4ed0e0ae131adcc9eaeb8c0fc5daa
Thijs Clowting <thijsclowting@hotmail.nl>
2014-12-18 19:03:03 +0100

    Initial Push

---
+++ src/test/SetupClass.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class SetupClass extends BasicGame {
+
+	public SetupClass(String title) {
+		super(title);
+	}
+	
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void render(GameContainer container, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
